79c087118ed6c6fdd8856673a6e6bafeb4fe62a9
evelinpereiracosta30 <evelinpereiracosta9@gmail.com>
2025-05-14 17:44:34 -0300

    Add files via upload

---
+++ bkp.sql
@@ -0,0 +1,12 @@
+CREATE TABLE hospede (
+	nome VARCHAR (25) NOT NULL,
+    genero VARCHAR (1),
+    biotipo VARCHAR (1),
+    altura NUMERIC (5,2),
+    PRIMARY KEY (nome)
+)
+
+SELECT nome, genero, biotipo, altura FROM hospede;
+
+INSERT INTO hospede (nome, genero, biotipo, autura;
+VALUES ('MIGUEL', 'M', 'M', 1.67)
